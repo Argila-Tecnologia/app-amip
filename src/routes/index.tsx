7b225fc48d3642dadb26f56ca0865347dac52294
Lubni Morais <lubni.morais@gmail.com>
2025-03-15 10:40:22 -0300

    chore: change slug from amip-tdm to amiptm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,28 +2,25 @@ import { View } from 'react-native';
 
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
-import { useAuth } from '@hooks/auth';
+// import { useAuth } from '@hooks/auth';
 
 import { useTheme } from 'styled-components/native';
 
 import { AppRoutes } from './app.routes';
-import { Loading } from '@components/Loading';
+// import { Loading } from '@components/Loading';
 
 export function Routes() {
-  const { isLoadingUserStorageData } = useAuth();
-  console.log(
-    '🚀 ~ Routes ~ isLoadingUserStorageData:',
-    isLoadingUserStorageData,
-  );
+  // const { player } = useAuth();
+  // console.log('🚀 ~ Routes ~ isLoadingUserStorageData:', player.id);
 
   const themeStyledComponents = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = themeStyledComponents.COLORS['gray-color-100'];
 
-  if (isLoadingUserStorageData) {
-    return <Loading />;
-  }
+  // if (!player.id) {
+  //   return <Loading />;
+  // }
 
   return (
     <View
